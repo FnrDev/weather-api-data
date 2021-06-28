a688830dabc6176de35c2afa5bbb91c72066482a
FnrDev <ah2014bh@outlook.com>
2021-06-28 18:43:44 +0300

    Update some codes in test file

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
 // using await
-const weather = require('weather-api-data');
+const weather = require('./index');
 
 async function getWeather() {
     const data = await weather.loction('manama');
     console.log(data);
 }
-
 getWeather();
-
 // using .then
 weather.loction('manama').then(weather => console.log(weather));
